c99f6b35e7034487581728e4e932ee9aed89a00d
Ania Oleksy <a.oleksy.m@gmail.com>
2023-10-17 18:05:19 +0200

    commiting old code, tb commented and changed

---
+++ project/showtimes/views.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+
+from showtimes.models import Cinema
+from showtimes.serializers import CinemaSerializer
+from showtimes.serializers import ScreeningSerializer
+from showtimes.models import Screening
+
+
+class CinemaListView(generics.ListCreateAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class CinemaView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class ScreeningListView(generics.ListCreateAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+
+
+class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
